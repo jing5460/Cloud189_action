44646f604c561d28aeda78115dfa483ff9822013
xtyuns <xtyuns@163.com>
2021-03-02 15:18:13 +0800

    fix a bug

--- action.py
+++ action.py
@@ -1,12 +1,11 @@
 import sys
 from cloud189 import *
-from functions import *
 
 
 def main(user: str, pwd: str):
     print_msg()
     print_msg(hide_username(user) + ":", True)
-    cloud = Client(user, pwd)
+    cloud = Client.Client(user, pwd)
     print_msg(cloud.msg)
     if not cloud.isLogin:
         exit(-1)
@@ -15,6 +14,19 @@ def main(user: str, pwd: str):
     cloud.draw()
     print_msg(cloud.msg)
     print_msg()
+
+
+def hide_username(name: str) -> str:
+    u_len = len(name)
+    fill_len = int(u_len * 0.3) + 1
+    b_index = int((u_len - fill_len) / 2)
+    e_index = u_len - fill_len - b_index
+    return name[:b_index] + "*" * fill_len + name[-e_index:]
+
+
+def print_msg(msg: str = "", isFirstLine: bool = False):
+    indent = " " * 4 if not isFirstLine else ""
+    print(indent + msg)
 
 
 if __name__ == '__main__':
