636cee3e2ba98fb69aca8591c468e69654f10380
xtyuns <xtyuns@163.com>
2021-03-17 15:40:58 +0800

    debug(action): test message push

--- extends/pushPlusPush.py
+++ extends/pushPlusPush.py
@@ -1,10 +1,11 @@
 import requests
 
 
-def push_text(msg, token):
+def push_text(msg: str, token: str):
     data = {
+        "template": "html",
         "token": token,
         "title": "cloud189app-action",
-        "content": msg
+        "content": msg.replace("\n", "<br>").replace(" ", "&nbsp;")
     }
     return requests.post("http://www.pushplus.plus/send", data=data).text
